c04255f5810e5d37760110026476a75d53863845
root <parth.social09@gmail.com>
2023-06-26 21:34:23 +0530

    init

--- app/src/main/java/com/example/datingapp/activity/EdtProfileActivity.kt
+++ app/src/main/java/com/example/datingapp/activity/EdtProfileActivity.kt
@@ -5,30 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.datingapp.R
-import com.example.datingapp.databinding.ActivitySelectMakeProfileBinding
+import com.example.datingapp.databinding.ActivityEdtProfileBinding
 
-class SelectMakeProfileActivity : ComponentActivity() {
-    private lateinit var binding: ActivitySelectMakeProfileBinding
+class EdtProfileActivity : ComponentActivity() {
+    private lateinit var binding: ActivityEdtProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_select_make_profile)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_edt_profile)
         init()
     }
     private fun init() {
+        binding.customeToolbar.title.text = "Profile"
         val window: Window = this.window
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.statusBarColor = ContextCompat.getColor(this, com.example.datingapp.R.color.white)
 
-        binding.makeProfile.setOnClickListener {
-            val i = Intent(this,EdtProfileActivity::class.java)
+        binding.customeToolbar.backBtn.setOnClickListener {
+            val i = Intent(this,SplashScreen::class.java)
             startActivity(i)
-            this.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+            this.overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left)
             finish()
         }
 
